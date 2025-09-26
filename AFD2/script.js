184b28e28215de5d4d1f798e4da7c865f1c2974c
Chromolous <alexmachadofm@gmail.com>
2025-09-26 16:36:45 -0300

    Atualizações do alek, muita gambiarra filho passar bem

--- AFD2/script.js
+++ AFD2/script.js
@@ -1,78 +1,117 @@
-function displayQ() {
-    var entrada = document.getElementById("conjunto_de_estados").value.replace(/\s+/g, "");
-    var conjunto = entrada.split(',');
-    //var conjunto = ["Apples", "Bananas", "Milk", "Bread"];
-    var saida = document.getElementById("Qs");
-    
+estados = []
+alfabeto = []
+estado_incial = ""
+estados_finais = []
 
-    conjunto.forEach(function(item) {
-        var elemento = document.createElement("li");
-        if(item != "") {
-            elemento.textContent = item;
-            saida.appendChild(elemento);
-        }
-        /*document.getElementById("conjunto_de_estados").on("keydown", function() {
-            var value = document.getElementById("conjunto_de_estados").val();
-            document.getElementById("Qs").text( value );
-         } ).trigger("keydown");*/
+canvas_x = 1000
+canvas_y = 1000
+
+canvas_item = 0
+canvas_spacing = 150
+
+function attTabelaFunc() {
+    let thead = document.createElement("thead");
+    let tbody = document.createElement("tbody");
+
+    estados.forEach(estado => {
+        let row = tbody.insertRow();
+        let h = document.createElement('th');
+        h.textContent = estado;
+        row.prepend(h)
+        alfabeto.forEach(_ => {
+            cell = row.insertCell()
+            cell.setAttribute("contenteditable", "true")
+        })
+
     });
-    /*document.getElementById("conjunto_de_estados").on("keydown", function() {
-        conjunto.forEach(function(item) {
-            var elemento = document.createElement("li");
-            if(item != "") {
-                elemento.textContent = item;
-                saida.appendChild(elemento);
-            }
-        });
-    } ).trigger("keydown");*/
-}
 
-function geraDelta(){
-    var verify = document.getElementById("conjunto_de_estados");
-    
-    verify.addEventListener("keydown", function(tecla) {
-        if(tecla.code === "Enter") {
-            var entrada = document.getElementById("conjunto_de_estados").value.replace(/\s+/g, "");
-            var conjunto = entrada.split(',');
+    let simbolo = document.createElement('th');
+    simbolo.setAttribute("style", "width: 2rem")
+    simbolo.textContent = "δ"
+    thead.appendChild(simbolo)
 
-            var tabela = document.getElementById("tabelaDelta");
-            var corpo = document.createElement("tbody");
-            for(let i = 0; i<conjunto.length; i++) {
-                var linha = document.createElement("tr");
-        
-                for(let j = 0; j<2; j++) {
-                    var cell = document.createElement("td");
-                    var cellText = document.createTextNode(`cell in row ${i}, column ${j}`);
-                    cell.appendChild(cellText);
-                    linha.appendChild(cell);
-                }
-                corpo.appendChild(linha);
-            }
-            tabela.appendChild(corpo);
-            tabela.setAttribute("border", "2");
-        }
-    });
-    
+    alfabeto.forEach(letra => {
+        let h = document.createElement('th');
+        h.textContent = letra
+        thead.appendChild(h)
+    })
 
-    
+    document.querySelector("#tabelaDelta tbody").replaceWith(tbody)
+    document.querySelector("#tabelaDelta thead").replaceWith(thead)
     
 }
 
-function printaInput(input) {
-    let entrada = document.getElementById(input);
-    let conjuntos = null;
-    if (input == "conjunto_de_estados") {
-        conjuntos = validaEstados();
+function desenhaEstados() {
+    const canvas = document.getElementById("tela");
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+
+    let posX = canvas_item+canvas_spacing
+    let posY = canvas_item+canvas_spacing
+
+    estados.forEach(estado => {
+        estadoDraw(estado, posX, posY)
+        posX = posX+canvas_spacing
+        if (posX > canvas_x-50) {
+            posY = posY+canvas_spacing
+            posX = canvas_item+canvas_spacing
+        }
+    })
+
+
+}
+
+function estadoDraw(nome, x, y) {
+    let final = estados_finais.includes(nome)
+
+    console.log(final)
+
+    const canvas = document.getElementById("tela");
+    const ctx = canvas.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(x, y, 50, 0, 2*Math.PI)
+    ctx.stroke();
+    if (final) {
+        ctx.arc(x, y, 45, 0, 2*Math.PI)
+        ctx.stroke();
     }
-    console.log(entrada.value);
-    console.log(conjuntos);
-    alert(entrada.value + conjuntos);
+    ctx.closePath();
+    ctx.font = "28px serif";
+    ctx.fillText(nome, x-12, y+5);
 }
+
 
 function validaEstados() {
     let entrada = document.getElementById("conjunto_de_estados");
     let formatado = entrada.value.replace(" ","");
     let conjunto = formatado.split(',');
 
+    estados = conjunto
+
     return conjunto
+}
+
+function validaAlfabeto() {
+    let entrada = document.getElementById("alfabeto");
+    let formatado = entrada.value.replace(" ","");
+    let conjunto = formatado.split(',');
+
+    alfabeto = conjunto
+
+    return conjunto
+}
+
+function validaFinais() {
+    let entrada = document.getElementById("conjunto_de_estados_finais");
+    let formatado = entrada.value.replace(" ","");
+    let conjunto = formatado.split(',');
+
+    estados_finais = conjunto
+
+    return conjunto
+}
+
+function recebeEInicial() {
+    estado_incial = documento.getElementById("estado_inicial").value
+
 }
